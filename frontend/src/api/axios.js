5f7d08c460db01ae7389c76ab2bfdcf12d0e1325
Sandeep S <sandeepfd78@gmail.com>
2026-03-26 11:35:14 +0530

    fix api base url

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: "https://writenow-full-stack-note-taking-website.onrender.com",
 });
 
 api.interceptors.request.use(
